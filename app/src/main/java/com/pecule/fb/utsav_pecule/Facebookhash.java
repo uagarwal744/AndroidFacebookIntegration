61d14c10c9c21fe41e7b454c6ea9aa547bf9954c
Utsav Agarwal <uagarwal744@gmail.com>
2016-03-13 21:54:28 +0530

    Initial Commit

---
+++ app/src/main/java/com/pecule/fb/utsav_pecule/Facebookhash.java
@@ -0,0 +1,39 @@
+package com.pecule.fb.utsav_pecule;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by utsav on 13/3/16.
+ */
+public class Facebookhash extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        printHashKey();
+    }
+
+    public void printHashKey() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.pecule.fb.utsav_pecule",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("COM:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+}
